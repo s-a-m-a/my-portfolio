94d5c6068e4a3324ebf6591140a2103f69169217
s-a-m-a <114341715+s-a-m-a@users.noreply.github.com>
2025-05-09 17:07:17 +0530

    updates

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,15 @@
 import { Outfit, Ovo } from "next/font/google";
+import Script from "next/script"; // Import Script component from Next.js
 import "./globals.css";
 
 const outfit = Outfit({
-  
-  subsets: ["latin"], weight: ["400", "500", "600", "700"]
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 const ovo = Ovo({
-
-  subsets: ["latin"],weight: ["400"]
+  subsets: ["latin"],
+  weight: ["400"],
 });
 
 export const metadata = {
@@ -18,10 +19,28 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth ">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark: text-white`}
       >
+        {/* Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-3GDJC6WQ52"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-3GDJC6WQ52');
+            `,
+          }}
+        />
+
         {children}
       </body>
     </html>
